9c398d5bd3887e2e7c1b8054b6bebe42e135060d
HaridhaRavi <haridha.gr@gmail.com>
2022-09-20 01:10:04 +0200

    create basic file structure and functionality

---
+++ js/main.js
@@ -0,0 +1,163 @@
+//creating dom elemnts
+const outerGrid = document.querySelector('.outer-grid');
+
+
+//Global variables and constants
+const blockWidth = 10;
+const blockHeight = 4;
+const ballDiameter =4;
+const outerGridWidth = 52;
+const outerGridHeight = 80;
+let ballXDirection = 1;
+let ballYDirection = 1;
+let intervalId;
+const userStart = [18,1]
+let PaddleCrntPosition = userStart;
+
+const ballStart =[23,5];
+let ballCurrentPosition = ballStart;
+
+
+class Block{
+    constructor(xAxis,yAxis){
+        this.bottomLeft = [xAxis, yAxis]
+        this.bottomRight = [xAxis+blockWidth, yAxis]
+        this.topLeft = [xAxis, yAxis+blockHeight]
+        this.topRight = [xAxis +blockWidth, yAxis+blockHeight]
+    }
+}    
+
+// xAxis - positionx+width; yAxis -postionY +height;
+const blocks = [
+    new Block(0,75),
+    new Block(10,75),
+    new Block(20,75),
+    new Block(30,75),
+    new Block(40,75),
+    new Block(0,70),
+    new Block(10,70),
+    new Block(20,70),
+    new Block(30,70),
+    new Block(40,70),
+    new Block(0,65),
+    new Block(10,65),
+    new Block(20,65),
+    new Block(30,65),
+    new Block(40,65),
+
+]
+function createBlock(){
+    for(let i=0;i<blocks.length;i++){
+        const block = document.createElement('div');
+        block.classList.add('block');
+        block.style.left = blocks[i].bottomLeft[0] +'vw';
+        block.style.bottom = blocks[i].bottomLeft[1] +'vh';
+        outerGrid.appendChild(block);
+
+    }
+    
+}
+createBlock();
+
+
+// create Paddle
+
+const paddle = document.createElement('div')
+paddle.classList.add('paddle');
+drawPaddle()
+outerGrid.appendChild(paddle)
+  
+
+function drawPaddle(){
+    paddle.style.left = PaddleCrntPosition[0]+'vw';
+    paddle.style.bottom = PaddleCrntPosition[1]+'vh';  
+}
+
+
+document.addEventListener('keydown',movePaddle)
+function movePaddle(e) {
+    switch (e.key) {
+      case 'ArrowLeft':
+        if(PaddleCrntPosition[0] > 0){
+            PaddleCrntPosition[0] -= 1;
+            console.log(PaddleCrntPosition[0] > 0)
+            drawPaddle();   
+        }
+        break;
+      case 'ArrowRight':
+        if(PaddleCrntPosition[0] < 48 - blockWidth){
+            PaddleCrntPosition[0] += 1;
+            console.log(PaddleCrntPosition[0] > 0)
+            drawPaddle();   
+        }
+        break;
+
+    }
+}
+
+
+//create ball
+
+const ball = document.createElement('div');
+ball.classList.add('ball');
+drawBall();
+outerGrid.appendChild(ball);
+
+function drawBall(){
+    ball.style.left = ballCurrentPosition[0]+'vw';
+    ball.style.bottom = ballCurrentPosition[1] +'vh';
+}
+
+function moveBall(){
+    ballCurrentPosition[0] += ballXDirection;
+    ballCurrentPosition[1] += ballYDirection;
+    drawBall();
+    checkForCollisions();
+}
+
+intervalId = setInterval(moveBall,100);
+
+
+//collisions check
+
+function checkForCollisions(){
+
+    //chcek block collision
+    for(let i=0;i<blocks.length;i++){
+
+    }
+
+
+    if((ballCurrentPosition[0] >= outerGridWidth - ballDiameter) ||
+    (ballCurrentPosition[1] >= outerGridHeight - ballDiameter) || 
+    (ballCurrentPosition[0] <= 0)) {
+        changeBallDirection();
+    }
+    if(ballCurrentPosition[1] <=0){
+        clearInterval(intervalId);
+        console.log("Game Over")
+        scoreDisplay.innerHTML = "you lose";
+        document.removeEventListener('keydown',movePaddle)
+    }
+}
+
+
+
+function changeBallDirection(){
+    if(ballXDirection === 1 && ballYDirection === 1){
+        ballXDirection = -1
+        return
+    }
+    if(ballXDirection === -1 && ballYDirection === 1){
+        ballYDirection = -1;
+        return
+    }
+    if(ballXDirection === -1 && ballYDirection === -1){
+        ballXDirection = 1;
+        return
+    }
+    if(ballXDirection === 1 && ballYDirection === -1){
+        ballYDirection = 1;
+        return
+    }
+}
